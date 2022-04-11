cd050c46b642756a5e77d62f96e4f76259427437
Denis Balandin <doublestar2013@yandex.ru>
2022-04-11 10:12:35 +0300

    Lesson2 First

--- NotesAPP/ViewController.swift
+++ NotesAPP/ViewController.swift
@@ -11,21 +11,46 @@ import UIKit
 class ViewController: UIViewController {
     private let textView = UITextView()
     private let defaults = UserDefaults.standard
-    private let firstTitle = UITextField()
-    private let myButton = UIBarButtonItem()
-    private let titleField = UITextField()
+    private let textField = UITextField()
 
+    private let myButton = UIBarButtonItem()
     private let mainTextView: UITextView = {
         let mainTextView = UITextView()
         mainTextView.translatesAutoresizingMaskIntoConstraints = false
         return mainTextView
     }()
 
+    private let titleTextField: UITextField = {
+        let titleTextField = UITextField()
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
+
+        return titleTextField
+    }()
+
+    let datePickField = UITextField()
+    let date = Date()
+    var holderDate = String()
+    struct EmptyCheck {
+        var emptyState = String()
+        var emptyString = String()
+        var empty: String? {
+            get {
+                return emptyState
+            }
+            set (newValue) {
+                if newValue?.isEmpty ?? true {
+                    emptyState = "Yes"
+                } else {
+                    emptyState = "No"
+                }
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         mainTextView.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 14)
-        firstTitle.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 22)
+        titleTextField.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 22)
 
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(
@@ -36,44 +61,167 @@ class ViewController: UIViewController {
         )
         let myButton = UIBarButtonItem(title: "Готово", style: .plain, target: self, action: #selector(stopFocusing))
 
-        firstTitle.placeholder = "Название заметки"
+        titleTextField.placeholder = "Название заметки"
+
+        setCurrentDate()
+
+        if datePickField.text != nil || datePickField.text != "" {
+            datePickField.text = defaults.string(forKey: "dateField")
+        }
 
         mainTextView.text = defaults.string(forKey: "mainText")
-        firstTitle.text = defaults.string(forKey: "topText")
+        titleTextField.text = defaults.string(forKey: "topText")
 
         view.addSubview(mainTextView)
-        view.addSubview(titleField)
         view.addSubview(textView)
-        view.addSubview(firstTitle)
+        view.addSubview(titleTextField)
+        view.addSubview(datePickField)
 
         view.backgroundColor = .white
         navigationItem.rightBarButtonItem = myButton
         setConstraints()
-        titleField.translatesAutoresizingMaskIntoConstraints = false
-        firstTitle.translatesAutoresizingMaskIntoConstraints = false
+        datePickField.translatesAutoresizingMaskIntoConstraints = false
         mainTextView.becomeFirstResponder()
+        self.datePickField.datePicker(
+            targer: self,
+            doneAction: #selector(doneAction),
+            cancelAction: #selector(cancelAction),
+            datePickerMode: .date
+        )
+    }
+
+    private func setCurrentDate() {
+        // var a = String()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d, YYYY"
+        holderDate = dateFormatter.string(from: date)
+        datePickField.placeholder = "\(holderDate)"
     }
 
     private func setConstraints() {
-        firstTitle.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-        firstTitle.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        firstTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        titleTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        titleTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        titleTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+
+        datePickField.topAnchor.constraint(equalTo: titleTextField.bottomAnchor).isActive = true
+        datePickField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        datePickField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+
+        datePickField.textAlignment = .center
 
         mainTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         mainTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         mainTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        mainTextView.topAnchor.constraint(equalTo: firstTitle.bottomAnchor).isActive = true
+        mainTextView.topAnchor.constraint(equalTo: datePickField.bottomAnchor).isActive = true
+        mainTextView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 
     @objc func stopFocusing() {
-        mainTextView.resignFirstResponder()
-        firstTitle.resignFirstResponder()
+        save()
+
+        mainTextView.text = defaults.string(forKey: "mainText")
+        titleTextField.text = defaults.string(forKey: "topText")
+
+        var emptyCck = EmptyCheck()
+        emptyCck.empty = mainTextView.text
+        let val1 = emptyCck.empty
+        emptyCck.empty = titleTextField.text
+        let val2 = emptyCck.empty
+
+        if val1 == "Yes" && val2 == "Yes" {
+            showEmptyNoteAlert(on: self)
+        } else {
+            mainTextView.resignFirstResponder()
+            titleTextField.resignFirstResponder()
+        }
     }
 
     @objc func save() {
         let mainText = mainTextView.text
-        let topView = firstTitle.text
+        let topView = titleTextField.text
+        let dateField = datePickField.text
         defaults.set(topView, forKey: "topText")
         defaults.set(mainText, forKey: "mainText")
+        defaults.set(dateField, forKey: "dateField")
+    }
+
+    @objc func cancelAction() {
+        self.datePickField.resignFirstResponder()
+    }
+
+    @objc func doneAction() {
+        if let datePickerView = self.datePickField.inputView as? UIDatePicker {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MMM d, YYY"
+            let dateString = dateFormatter.string(from: datePickerView.date)
+            self.datePickField.text = dateString
+            self.datePickField.resignFirstResponder()
+        }
+    }
+}
+
+extension UITextField {
+    func datePicker<T>(
+        targer: T,
+        doneAction: Selector,
+        cancelAction: Selector,
+        datePickerMode: UIDatePicker.Mode = .date
+    ) {
+        let screenWidth = UIScreen.main.bounds.width
+        func buttonItem(withSystemItemStyle style: UIBarButtonItem.SystemItem) -> UIBarButtonItem {
+            let buttonTarget = style == .flexibleSpace ? nil : targer
+            let action: Selector? = {
+                switch style {
+                case .cancel:
+                    return cancelAction
+                case .done:
+                    return doneAction
+                default:
+                    return nil
+                }
+            }()
+
+            let barButtonItem = UIBarButtonItem(
+                barButtonSystemItem: style,
+                target: buttonTarget,
+                action: action
+            )
+            return barButtonItem
+        }
+
+        let datePicker = UIDatePicker(frame: CGRect(
+            x: 0,
+            y: 0,
+            width: screenWidth,
+            height: 216
+        ))
+        datePicker.datePickerMode = datePickerMode
+        self.inputView = datePicker
+        let toolBar = UIToolbar(frame: CGRect(
+            x: 0,
+            y: 0,
+            width: screenWidth,
+            height: 44
+        ))
+        toolBar.setItems(
+            [buttonItem(withSystemItemStyle: .cancel),
+             buttonItem(withSystemItemStyle: .flexibleSpace),
+             buttonItem(withSystemItemStyle: .done)],
+            animated: true
+        )
+        self.inputAccessoryView = toolBar
+    }
+}
+
+extension UIViewController {
+    func showAlertt(on viewController: UIViewController, with title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        DispatchQueue.main.async {
+            viewController.present(alert, animated: true, completion: nil)
+        }
+    }
+    func showEmptyNoteAlert (on viewController: UIViewController) {
+        showAlertt(on: viewController, with: "ERROR", message: "Your note is empty!")
     }
 }
